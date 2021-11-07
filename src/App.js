8bc7960242ae47cf0a3747a2ea865a8fcc4a7fae
PumbaSD <dudu32769144@gmail.com>
2021-11-06 21:40:45 -0300

    Cicle Methods and Fetching

--- src/App.js
+++ src/App.js
@@ -1,68 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component{
   state = {
-    name: 'Paulo Eduardo',
-    counter: 0
+    posts: []
   };
-  
-  handlePClick = () => {
-    this.setState({name: 'Carlos Eduardo'})
+
+  componentDidMount(){
+    this.loadPosts();
   }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter + 1})
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    
+    const [posts, photos] = await Promise.all([postsResponse , photosResponse]);
+    
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((posts, index) => {
+      return { ...posts, cover: photosJson[index].url}
+    })
+
+    this.setState({ posts: postsAndPhotos })
   }
 
   render (){
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick = {this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick = {this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o Link!
-          </a>
-        </header>
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1> { post.title }</h1>
+                <p> { post.body } </p>
+              </div>
+            </div>
+            
+          ))}       
+        </div>
+      </section>
+      
     );
   }
 
 }
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 export default App;
